00c4f02a6534d9385885182970b9ecfdc688597c
Gabriel Barreto <gabrielbrt.dev@gmail.com>
2024-01-23 16:53:26 -0300

    Configuração do ambiente de Testes.

---
+++ Test/Controllers.Conexao.pas
@@ -0,0 +1,117 @@
+unit Controllers.Conexao;
+
+interface
+  uses
+    FireDAC.Comp.Client,
+    Interfaces.Conexao;
+
+  type
+    TControllersConexao = class(TInterfacedObject, IConexao)
+
+      private
+        function criarQuery                              : TFDQuery;
+        function criarQueryExterna                       : TFDQuery;
+        function ConfigConexaoExterna(pLoja: String)     : Boolean;
+        function Generetor(pValue: String)               : String;
+        function getConnection                           : TFDConnection;
+        function getLojaConectada                        : String;
+        function DataServer                              : TDate;
+        function HoraServer                              : TTime;
+
+        function getUSer                                 : TUsuario;
+        function setUser(pUser : TUsuario)               : Boolean;
+
+        function setEmpresa(pEmpresa: TEmpresa)          : Boolean;
+        function getEmpresa                              : TEmpresa;
+
+      public
+        Constructor Create;
+        Destructor Destroy; override;
+        Class function New: IConexao;
+
+
+    end;
+
+implementation
+
+uses
+  uTeste;
+
+{ TControllersConexao }
+
+function TControllersConexao.ConfigConexaoExterna(pLoja: String): Boolean;
+begin
+
+end;
+
+constructor TControllersConexao.Create;
+begin
+
+end;
+
+function TControllersConexao.criarQuery: TFDQuery;
+begin
+  Result := Form1.vConexao.criarQuery;
+end;
+
+function TControllersConexao.criarQueryExterna: TFDQuery;
+begin
+  Result := Form1.vConexao.criarQueryExterna;
+end;
+
+function TControllersConexao.DataServer: TDate;
+begin
+  Result := Form1.vConexao.DataServer;
+end;
+
+destructor TControllersConexao.Destroy;
+begin
+  inherited;
+end;
+
+function TControllersConexao.Generetor(pValue: String): String;
+begin
+  Result := Form1.vConexao.Generetor(pValue);
+end;
+
+function TControllersConexao.getConnection: TFDConnection;
+begin
+  Result := Form1.vConexao.getConnection;
+end;
+
+function TControllersConexao.getEmpresa: TEmpresa;
+begin
+
+end;
+
+function TControllersConexao.getLojaConectada: String;
+begin
+
+end;
+
+function TControllersConexao.getUSer: TUsuario;
+begin
+
+end;
+
+function TControllersConexao.HoraServer: TTime;
+begin
+  Result := Form1.vConexao.HoraServer;
+end;
+
+class function TControllersConexao.New: IConexao;
+begin
+  Result := Self.Create;
+end;
+
+function TControllersConexao.setEmpresa(pEmpresa: TEmpresa): Boolean;
+begin
+
+end;
+
+function TControllersConexao.setUser(pUser: TUsuario): Boolean;
+begin
+
+end;
+
+end.
--- Test/uTeste.dfm
+++ Test/uTeste.dfm
@@ -2,25 +2,74 @@ object Form1: TForm1
   Left = 0
   Top = 0
   Caption = 'Form1'
-  ClientHeight = 670
-  ClientWidth = 982
+  ClientHeight = 741
+  ClientWidth = 1248
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
   Font.Height = -12
   Font.Name = 'Segoe UI'
   Font.Style = []
+  Position = poScreenCenter
+  OnCreate = FormCreate
   TextHeight = 15
   object PageControl1: TPageControl
     Left = 0
     Top = 0
-    Width = 982
-    Height = 670
+    Width = 1248
+    Height = 741
     ActivePage = TabSheet1
     Align = alClient
     TabOrder = 0
+    ExplicitWidth = 982
+    ExplicitHeight = 670
     object TabSheet1: TTabSheet
       Caption = 'Teste 1'
+      object memoResultado: TMemo
+        Left = 816
+        Top = 15
+        Width = 409
+        Height = 681
+        Lines.Strings = (
+          'memoResultado')
+        TabOrder = 0
+      end
+      object btnFinanceiroPedido: TButton
+        Left = 3
+        Top = 15
+        Width = 182
+        Height = 42
+        Caption = 'Financeiro Pedido Consultar'
+        TabOrder = 1
+        OnClick = btnFinanceiroPedidoClick
+      end
+      object Button1: TButton
+        Left = 191
+        Top = 15
+        Width = 182
+        Height = 42
+        Caption = 'Financeiro Pedido Inserir'
+        TabOrder = 2
+        OnClick = Button1Click
+      end
+      object Button2: TButton
+        Left = 379
+        Top = 15
+        Width = 182
+        Height = 42
+        Caption = 'Financeiro Pedido Update'
+        TabOrder = 3
+        OnClick = Button2Click
+      end
     end
+  end
+  object Button3: TButton
+    Left = 571
+    Top = 41
+    Width = 182
+    Height = 42
+    Caption = 'Financeiro Pedido Excluir'
+    TabOrder = 1
+    OnClick = Button3Click
   end
 end
