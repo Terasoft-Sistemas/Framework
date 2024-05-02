de25bc106d07e0a7a26c7f54dda48c8337142848
Gabriel Barreto <gabrielbrt.dev@gmail.com>
2024-05-02 17:04:00 -0300

    Criação de Classes SaidasItens.

---
+++ Dao/SaidasItensDao.pas
@@ -0,0 +1,380 @@
+unit SaidasItensDao;
+
+interface
+
+uses
+  SaidasItensModel,
+  FireDAC.Comp.Client,
+  System.SysUtils,
+  System.StrUtils,
+  System.Generics.Collections,
+  System.Variants,
+  Interfaces.Conexao,
+  Terasoft.Utils,
+  Terasoft.ConstrutorDao;
+
+type
+  TSaidasItensDao = class
+
+  private
+    vIConexao   : IConexao;
+    vConstrutor : TConstrutorDao;
+
+    FLengthPageView: String;
+    FIDRecordView: Integer;
+    FStartRecordView: String;
+    FID: Variant;
+    FCountView: String;
+    FOrderView: String;
+    FWhereView: String;
+    FTotalRecords: Integer;
+    FNumeroSaidaView: String;
+    procedure obterTotalRegistros;
+    procedure SetCountView(const Value: String);
+    procedure SetID(const Value: Variant);
+    procedure SetIDRecordView(const Value: Integer);
+    procedure SetLengthPageView(const Value: String);
+    procedure SetOrderView(const Value: String);
+    procedure SetStartRecordView(const Value: String);
+    procedure SetTotalRecords(const Value: Integer);
+    procedure SetWhereView(const Value: String);
+
+    function where: String;
+    procedure SetNumeroSaidaView(const Value: String);
+
+  public
+
+    constructor Create(pIConexao : IConexao);
+    destructor Destroy; override;
+
+    property ID :Variant read FID write SetID;
+    property TotalRecords: Integer read FTotalRecords write SetTotalRecords;
+    property WhereView: String read FWhereView write SetWhereView;
+    property CountView: String read FCountView write SetCountView;
+    property OrderView: String read FOrderView write SetOrderView;
+    property StartRecordView: String read FStartRecordView write SetStartRecordView;
+    property LengthPageView: String read FLengthPageView write SetLengthPageView;
+    property IDRecordView: Integer read FIDRecordView write SetIDRecordView;
+    property NumeroSaidaView : String read FNumeroSaidaView write SetNumeroSaidaView;
+
+    function incluir(pSaidasItensModel: TSaidasItensModel): String;
+    function alterar(pSaidasItensModel: TSaidasItensModel): String;
+    function excluir(pSaidasItensModel: TSaidasItensModel): String;
+    function carregaClasse(pID : String): TSaidasItensModel;
+    function obterLista: TFDMemTable;
+
+    function ObterTotais(pNmeroSaida : String) : TFDMemTable;
+    procedure setParams(var pQry: TFDQuery; pSaidasItensModel: TSaidasItensModel);
+
+end;
+
+implementation
+
+uses
+  System.Rtti;
+
+{ TSaidasItens }
+
+function TSaidasItensDao.carregaClasse(pID : String): TSaidasItensModel;
+var
+  lQry : TFDQuery;
+  lModel : TSaidasItensModel;
+begin
+  lQry     := vIConexao.CriarQuery;
+  lModel   := TSaidasItensModel.Create(vIConexao);
+  Result   := lModel;
+
+  try
+    lQry.Open('select * from saidasitens where numero_sai = ' +pID);
+
+    if lQry.IsEmpty then
+      Exit;
+
+    lModel.NUMERO_SAI     := lQry.FieldByName('NUMERO_SAI').AsString;
+    lModel.CODIGO_CLI     := lQry.FieldByName('CODIGO_CLI').AsString;
+    lModel.CODIGO_PRO     := lQry.FieldByName('CODIGO_PRO').AsString;
+    lModel.QUANTIDADE_SAI := lQry.FieldByName('QUANTIDADE_SAI').AsString;
+    lModel.QUANTIDADE_ATE := lQry.FieldByName('QUANTIDADE_ATE').AsString;
+    lModel.VALOR_UNI_SAI  := lQry.FieldByName('VALOR_UNI_SAI').AsString;
+    lModel.IPI_SAI        := lQry.FieldByName('IPI_SAI').AsString;
+    lModel.ICMS_SAI       := lQry.FieldByName('ICMS_SAI').AsString;
+    lModel.STATUS         := lQry.FieldByName('STATUS').AsString;
+    lModel.LOJA           := lQry.FieldByName('LOJA').AsString;
+    lModel.ID             := lQry.FieldByName('ID').AsString;
+    lModel.RESERVA_ID     := lQry.FieldByName('RESERVA_ID').AsString;
+    lModel.AVULSO         := lQry.FieldByName('AVULSO').AsString;
+    lModel.DESCONTO_PED   := lQry.FieldByName('DESCONTO_PED').AsString;
+    lModel.ALTURA_M       := lQry.FieldByName('ALTURA_M').AsString;
+    lModel.LARGURA_M      := lQry.FieldByName('LARGURA_M').AsString;
+    lModel.PROFUNDIDADE_M := lQry.FieldByName('PROFUNDIDADE_M').AsString;
+    lModel.DATA_CAD       := lQry.FieldByName('DATA_CAD').AsString;
+    lModel.QTD_CHECAGEM   := lQry.FieldByName('QTD_CHECAGEM').AsString;
+    lModel.REPOSICAO_ID   := lQry.FieldByName('REPOSICAO_ID').AsString;
+    lModel.PRODUCAO_ID    := lQry.FieldByName('PRODUCAO_ID').AsString;
+    lModel.SAIDA_ID       := lQry.FieldByName('SAIDA_ID').AsString;
+    lModel.SYSTIME        := lQry.FieldByName('SYSTIME').AsString;
+
+    Result := lModel;
+
+  finally
+    lQry.Free;
+  end;
+end;
+
+constructor TSaidasItensDao.Create(pIConexao : IConexao);
+begin
+  vIConexao := pIConexao;
+  vConstrutor := TConstrutorDAO.Create(vIConexao);
+end;
+
+destructor TSaidasItensDao.Destroy;
+begin
+  inherited;
+end;
+
+function TSaidasItensDao.incluir(pSaidasItensModel: TSaidasItensModel): String;
+var
+  lQry : TFDQuery;
+  lSQL : String;
+begin
+  lQry := vIConexao.CriarQuery;
+  lSQL := vConstrutor.gerarInsert('SAIDASITENS', 'NUMERO_SAI');
+  try
+    lQry.SQL.Add(lSQL);
+    setParams(lQry, pSaidasItensModel);
+    lQry.Open;
+
+    Result := lQry.FieldByName('NUMERO_SAI').AsString;
+  finally
+    lSQL := '';
+    lQry.Free;
+  end;
+end;
+
+function TSaidasItensDao.alterar(pSaidasItensModel: TSaidasItensModel): String;
+var
+  lQry : TFDQuery;
+  lSQL : String;
+begin
+
+  lQry := vIConexao.CriarQuery;
+  lSQL :=  vConstrutor.gerarUpdate('SAIDASITENS','ID');
+  try
+    lQry.SQL.Add(lSQL);
+    setParams(lQry, pSaidasItensModel);
+    lQry.ExecSQL;
+
+    Result := pSaidasItensModel.ID;
+
+  finally
+    lSQL := '';
+    lQry.Free;
+  end;
+end;
+
+function TSaidasItensDao.excluir(pSaidasItensModel: TSaidasItensModel): String;
+var
+  lQry: TFDQuery;
+begin
+  lQry := vIConexao.CriarQuery;
+
+  try
+   lQry.ExecSQL('delete from saidasitens where numero_sai = :numero_sai' ,[pSaidasItensModel.NUMERO_SAI]);
+   lQry.ExecSQL;
+   Result := pSaidasItensModel.ID;
+
+  finally
+    lQry.Free;
+  end;
+end;
+
+function TSaidasItensDao.where: String;
+var
+  lSQL : String;
+begin
+  lSQL := '';
+
+  if not FWhereView.IsEmpty then
+    lSQL := lSQL + FWhereView;
+
+  if FIDRecordView <> 0  then
+    lSQL := lSQL + ' and saidasitens.id = '+IntToStr(FIDRecordView);
+
+  if NumeroSaidaView <> '' then
+    lSQL := lSQL + ' and saidasitens.numero_sai = '+ QuotedStr(FNumeroSaidaView);
+
+  Result := lSQL;
+end;
+
+procedure TSaidasItensDao.obterTotalRegistros;
+var
+  lQry: TFDQuery;
+  lSQL:String;
+begin
+  try
+    lQry := vIConexao.CriarQuery;
+
+    lSql := 'select count(*) records from saidasitens where 1=1 ';
+
+    lSql := lSql + where;
+
+    lQry.Open(lSQL);
+
+    FTotalRecords := lQry.FieldByName('records').AsInteger;
+
+  finally
+    lQry.Free;
+  end;
+end;
+
+function TSaidasItensDao.obterLista: TFDMemTable;
+var
+  lQry       : TFDQuery;
+  lSQL       : String;
+  lPaginacao : String;
+begin
+  lQry := vIConexao.CriarQuery;
+
+  try
+    if (StrToIntDef(LengthPageView, 0) > 0) or (StrToIntDef(StartRecordView, 0) > 0) then
+      lPaginacao := ' first ' + LengthPageView + ' SKIP ' + StartRecordView + '';
+
+    lSql := ' select '+lPaginacao+'                                               '+SLineBreak+
+            '        saidasitens.id,                                              '+SLineBreak+
+            '        saidasitens.numero_sai,                                      '+SLineBreak+
+            '        saidasitens.codigo_cli,                                      '+SLineBreak+
+            '        saidasitens.codigo_pro,                                      '+SLineBreak+
+            '        produto.nome_pro produto_nome,                               '+SLineBreak+
+            '        produto.unidade_pro produto_unidade,                         '+SLineBreak+
+            '        saidasitens.quantidade_sai,                                  '+SLineBreak+
+            '        saidasitens.quantidade_ate,                                  '+SLineBreak+
+            '        saidasitens.valor_uni_sai,                                   '+SLineBreak+
+            '        saidasitens.status,                                          '+SLineBreak+
+            '        saidasitens.loja,                                            '+SLineBreak+
+            '        saidasitens.desconto_ped,                                    '+SLineBreak+
+            '        saidasitens.data_cad,                                        '+SLineBreak+
+            '        saidasitens.saida_id                                         '+SLineBreak+
+            '   from saidasitens                                                  '+SLineBreak+
+            '   left join produto on produto.codigo_pro = saidasitens.codigo_pro  '+SLineBreak+
+            '  where 1=1                                                          '+SLineBreak;
+
+    lSql := lSql + Where;
+
+    if not FOrderView.IsEmpty then
+      lSql := lSql + ' order by '+FOrderView;
+
+    lQry.Open(lSql);
+
+    Result := vConstrutor.atribuirRegistros(lQry);
+
+    obterTotalRegistros;
+
+  finally
+    lQry.Free;
+  end;
+end;
+
+function TSaidasItensDao.ObterTotais(pNmeroSaida : String): TFDMemTable;
+var
+  lQry : TFDQuery;
+  lSql : String;
+begin
+  lQry := vIConexao.CriarQuery;
+  try
+    lSql := ' select                                                                                   '+SLineBreak+
+            '        CUSTO,                                                                            '+SLineBreak+
+            '        VALOR                                                                             '+SLineBreak+
+            '   from                                                                                   '+SLineBreak+
+            '   (select                                                                                '+SLineBreak+
+            '           sum(quantidade * (custo - custo * percentual_desconto)) custo,                 '+SLineBreak+
+            '           sum(quantidade * (valor_saida - valor_saida * percentual_desconto)) valor      '+SLineBreak+
+            '      from                                                                                '+SLineBreak+
+            '      (select                                                                             '+SLineBreak+
+            '              cast(i.quantidade_sai  as float) quantidade,                                '+SLineBreak+
+            '              coalesce(i.icms_sai,0) custo,                                               '+SLineBreak+
+            '              coalesce(i.desconto_ped,0) / 100 percentual_desconto,                       '+SLineBreak+
+            '              coalesce(i.valor_uni_sai,0) valor_saida                                     '+SLineBreak+
+            '         from saidas s                                                                    '+SLineBreak+
+            '        inner join saidasitens i on i.numero_sai = s.numero_sai                           '+SLineBreak+
+            '        where s.numero_sai = '+QuotedStr(pNmeroSaida)+'                                   '+SLineBreak+
+            '       )                                                                                  '+SLineBreak+
+            '    )                                                                                     '+SLineBreak;
+
+    lQry.Open(lSql);
+
+    Result := vConstrutor.atribuirRegistros(lQry);
+  finally
+    lQry.Free;
+  end;
+end;
+
+procedure TSaidasItensDao.SetCountView(const Value: String);
+begin
+  FCountView := Value;
+end;
+
+procedure TSaidasItensDao.SetID(const Value: Variant);
+begin
+  FID := Value;
+end;
+
+procedure TSaidasItensDao.SetIDRecordView(const Value: Integer);
+begin
+  FIDRecordView := Value;
+end;
+
+procedure TSaidasItensDao.SetLengthPageView(const Value: String);
+begin
+  FLengthPageView := Value;
+end;
+
+procedure TSaidasItensDao.SetNumeroSaidaView(const Value: String);
+begin
+  FNumeroSaidaView := Value;
+end;
+
+procedure TSaidasItensDao.SetOrderView(const Value: String);
+begin
+  FOrderView := Value;
+end;
+
+procedure TSaidasItensDao.setParams(var pQry: TFDQuery; pSaidasItensModel: TSaidasItensModel);
+var
+  lTabela : TFDMemTable;
+  lCtx    : TRttiContext;
+  lProp   : TRttiProperty;
+  i       : Integer;
+begin
+  lTabela := vConstrutor.getColumns('SAIDASITENS');
+
+  lCtx := TRttiContext.Create;
+  try
+    for i := 0 to pQry.Params.Count - 1 do
+    begin
+      lProp := lCtx.GetType(TSaidasItensModel).GetProperty(pQry.Params[i].Name);
+
+      if Assigned(lProp) then
+        pQry.ParamByName(pQry.Params[i].Name).Value := IIF(lProp.GetValue(pSaidasItensModel).AsString = '',
+        Unassigned, vConstrutor.getValue(lTabela, pQry.Params[i].Name, lProp.GetValue(pSaidasItensModel).AsString))
+    end;
+  finally
+    lCtx.Free;
+  end;
+end;
+
+procedure TSaidasItensDao.SetStartRecordView(const Value: String);
+begin
+  FStartRecordView := Value;
+end;
+
+procedure TSaidasItensDao.SetTotalRecords(const Value: Integer);
+begin
+  FTotalRecords := Value;
+end;
+
+procedure TSaidasItensDao.SetWhereView(const Value: String);
+begin
+  FWhereView := Value;
+end;
+
+end.
---
+++ Model/SaidasItensModel.pas
@@ -0,0 +1,443 @@
+unit SaidasItensModel;
+
+interface
+
+uses
+  Terasoft.Types,
+  System.Generics.Collections,
+  Interfaces.Conexao,
+  FireDAC.Comp.Client;
+
+type
+
+  TSaidasItensModel = class
+
+  private
+  
+    vIConexao : IConexao;
+
+    FAcao: TAcao;
+    FLengthPageView: String;
+    FIDRecordView: Integer;
+    FStartRecordView: String;
+    FCountView: String;
+    FOrderView: String;
+    FWhereView: String;
+    FTotalRecords: Integer;
+    FID: Variant;
+    FSYSTIME: Variant;
+    FICMS_SAI: Variant;
+    FQTD_CHECAGEM: Variant;
+    FAVULSO: Variant;
+    FALTURA_M: Variant;
+    FNUMERO_SAI: Variant;
+    FRESERVA_ID: Variant;
+    FLARGURA_M: Variant;
+    FDESCONTO_PED: Variant;
+    FQUANTIDADE_SAI: Variant;
+    FCODIGO_CLI: Variant;
+    FDATA_CAD: Variant;
+    FLOJA: Variant;
+    FSTATUS: Variant;
+    FCODIGO_PRO: Variant;
+    FVALOR_UNI_SAI: Variant;
+    FREPOSICAO_ID: Variant;
+    FIPI_SAI: Variant;
+    FPRODUCAO_ID: Variant;
+    FPROFUNDIDADE_M: Variant;
+    FSAIDA_ID: Variant;
+    FQUANTIDADE_ATE: Variant;
+    FNumeroSaidaView: String;
+
+    procedure SetAcao(const Value: TAcao);
+    procedure SetCountView(const Value: String);
+    procedure SetIDRecordView(const Value: Integer);
+    procedure SetLengthPageView(const Value: String);
+    procedure SetOrderView(const Value: String);
+    procedure SetStartRecordView(const Value: String);
+    procedure SetTotalRecords(const Value: Integer);
+    procedure SetWhereView(const Value: String);
+    procedure SetID(const Value: Variant);
+    procedure SetALTURA_M(const Value: Variant);
+    procedure SetAVULSO(const Value: Variant);
+    procedure SetCODIGO_CLI(const Value: Variant);
+    procedure SetCODIGO_PRO(const Value: Variant);
+    procedure SetDATA_CAD(const Value: Variant);
+    procedure SetDESCONTO_PED(const Value: Variant);
+    procedure SetICMS_SAI(const Value: Variant);
+    procedure SetIPI_SAI(const Value: Variant);
+    procedure SetLARGURA_M(const Value: Variant);
+    procedure SetLOJA(const Value: Variant);
+    procedure SetNUMERO_SAI(const Value: Variant);
+    procedure SetPRODUCAO_ID(const Value: Variant);
+    procedure SetPROFUNDIDADE_M(const Value: Variant);
+    procedure SetQTD_CHECAGEM(const Value: Variant);
+    procedure SetQUANTIDADE_ATE(const Value: Variant);
+    procedure SetQUANTIDADE_SAI(const Value: Variant);
+    procedure SetREPOSICAO_ID(const Value: Variant);
+    procedure SetRESERVA_ID(const Value: Variant);
+    procedure SetSAIDA_ID(const Value: Variant);
+    procedure SetSTATUS(const Value: Variant);
+    procedure SetSYSTIME(const Value: Variant);
+    procedure SetVALOR_UNI_SAI(const Value: Variant);
+    procedure SetNumeroSaidaView(const Value: String);
+
+  public
+
+    property NUMERO_SAI     : Variant read FNUMERO_SAI write SetNUMERO_SAI;
+    property CODIGO_CLI     : Variant read FCODIGO_CLI write SetCODIGO_CLI;
+    property CODIGO_PRO     : Variant read FCODIGO_PRO write SetCODIGO_PRO;
+    property QUANTIDADE_SAI : Variant read FQUANTIDADE_SAI write SetQUANTIDADE_SAI;
+    property QUANTIDADE_ATE : Variant read FQUANTIDADE_ATE write SetQUANTIDADE_ATE;
+    property VALOR_UNI_SAI  : Variant read FVALOR_UNI_SAI write SetVALOR_UNI_SAI;
+    property IPI_SAI        : Variant read FIPI_SAI write SetIPI_SAI;
+    property ICMS_SAI       : Variant read FICMS_SAI write SetICMS_SAI;
+    property STATUS         : Variant read FSTATUS write SetSTATUS;
+    property LOJA           : Variant read FLOJA write SetLOJA;
+    property ID             : Variant read FID write SetID;
+    property RESERVA_ID     : Variant read FRESERVA_ID write SetRESERVA_ID;
+    property AVULSO         : Variant read FAVULSO write SetAVULSO;
+    property DESCONTO_PED   : Variant read FDESCONTO_PED write SetDESCONTO_PED;
+    property ALTURA_M       : Variant read FALTURA_M write SetALTURA_M;
+    property LARGURA_M      : Variant read FLARGURA_M write SetLARGURA_M;
+    property PROFUNDIDADE_M : Variant read FPROFUNDIDADE_M write SetPROFUNDIDADE_M;
+    property DATA_CAD       : Variant read FDATA_CAD write SetDATA_CAD;
+    property QTD_CHECAGEM   : Variant read FQTD_CHECAGEM write SetQTD_CHECAGEM;
+    property REPOSICAO_ID   : Variant read FREPOSICAO_ID write SetREPOSICAO_ID;
+    property PRODUCAO_ID    : Variant read FPRODUCAO_ID write SetPRODUCAO_ID;
+    property SAIDA_ID       : Variant read FSAIDA_ID write SetSAIDA_ID;
+    property SYSTIME        : Variant read FSYSTIME write SetSYSTIME;
+
+    constructor Create(pIConexao : IConexao);
+    destructor Destroy; override;
+
+    property Acao :TAcao read FAcao write SetAcao;
+    property TotalRecords: Integer read FTotalRecords write SetTotalRecords;
+    property WhereView: String read FWhereView write SetWhereView;
+    property CountView: String read FCountView write SetCountView;
+    property OrderView: String read FOrderView write SetOrderView;
+    property StartRecordView: String read FStartRecordView write SetStartRecordView;
+    property LengthPageView: String read FLengthPageView write SetLengthPageView;
+    property IDRecordView: Integer read FIDRecordView write SetIDRecordView;
+    property NumeroSaidaView : String read FNumeroSaidaView write SetNumeroSaidaView;
+
+    function Incluir: String;
+    function Alterar(pID : String): TSaidasItensModel;
+    function Excluir(pID : String): String;
+    function Salvar : String;
+    function carregaClasse(pID : String): TSaidasItensModel;
+    function obterLista: TFDMemTable;
+
+    procedure getDadosProduto;
+    function ObterTotais(pNmeroSaida : String) : TFDMemTable;
+
+  end;
+
+implementation
+
+uses
+  SaidasItensDao,  
+  System.Classes, 
+  System.SysUtils, SaidasModel, ProdutosModel, Terasoft.Configuracoes,
+  System.Rtti;
+
+{ TSaidasItensModel }
+
+function TSaidasItensModel.Alterar(pID: String): TSaidasItensModel;
+var
+  lSaidasItensModel : TSaidasItensModel;
+begin
+  lSaidasItensModel := TSaidasItensModel.Create(vIConexao);
+  try
+    lSaidasItensModel       := lSaidasItensModel.carregaClasse(pID);
+    lSaidasItensModel.Acao  := tacAlterar;
+    Result                  := lSaidasItensModel;
+  finally
+  end;
+end;
+
+function TSaidasItensModel.Excluir(pID: String): String;
+begin
+  self.ID      := pID;
+  self.FAcao   := tacExcluir;
+  Result       := self.Salvar;
+end;
+
+procedure TSaidasItensModel.getDadosProduto;
+var
+  lProdutosModel : TProdutosModel;
+  lConfiguracoes : TerasoftConfiguracoes;
+  lBaseCusto     : String;
+  lCtx           : TRttiContext;
+  lProp          : TRttiProperty;
+begin
+  if self.FCODIGO_PRO = '' then
+    Exit;
+
+  lProdutosModel := TProdutosModel.Create(vIConexao);
+  lCtx           := TRttiContext.Create;
+  try
+    lProdutosModel.IDRecordView := self.FCODIGO_PRO;
+    lProdutosModel.obterLista;
+    lProdutosModel := lProdutosModel.ProdutossLista[0];
+
+    lConfiguracoes := vIConexao.getTerasoftConfiguracoes as TerasoftConfiguracoes;
+
+    lBaseCusto := lConfiguracoes.valorTag('BASE_CUSTO_PADRAO', 'CUSTOMEDIO_PRO', tvString);
+
+    lProp := lCtx.GetType(TProdutosModel).GetProperty(lBaseCusto);
+
+    self.FICMS_SAI      := lProp.GetValue(lProdutosModel).AsString;
+    self.FVALOR_UNI_SAI := lProp.GetValue(lProdutosModel).AsString;
+
+  finally
+    lProdutosModel.Free;
+  end;
+end;
+
+function TSaidasItensModel.Incluir: String;
+begin
+  self.Acao := tacIncluir;
+  Result    := self.Salvar;
+end;
+
+function TSaidasItensModel.carregaClasse(pID : String): TSaidasItensModel;
+var
+  lSaidasItensDao: TSaidasItensDao;
+begin
+  lSaidasItensDao := TSaidasItensDao.Create(vIConexao);
+
+  try
+    Result := lSaidasItensDao.carregaClasse(pID);
+  finally
+    lSaidasItensDao.Free;
+  end;
+end;
+
+constructor TSaidasItensModel.Create(pIConexao : IConexao);
+begin
+  vIConexao := pIConexao;
+end;
+
+destructor TSaidasItensModel.Destroy;
+begin
+  inherited;
+end;
+
+function TSaidasItensModel.obterLista: TFDMemTable;
+var
+  lSaidasItensLista: TSaidasItensDao;
+begin
+  lSaidasItensLista := TSaidasItensDao.Create(vIConexao);
+
+  try
+    lSaidasItensLista.TotalRecords    := FTotalRecords;
+    lSaidasItensLista.WhereView       := FWhereView;
+    lSaidasItensLista.CountView       := FCountView;
+    lSaidasItensLista.OrderView       := FOrderView;
+    lSaidasItensLista.StartRecordView := FStartRecordView;
+    lSaidasItensLista.LengthPageView  := FLengthPageView;
+    lSaidasItensLista.IDRecordView    := FIDRecordView;
+
+    Result := lSaidasItensLista.obterLista;
+
+    FTotalRecords := lSaidasItensLista.TotalRecords;
+
+  finally
+    lSaidasItensLista.Free;
+  end;
+end;
+
+function TSaidasItensModel.ObterTotais(pNmeroSaida : String): TFDMemTable;
+var
+  lSaidasItens: TSaidasItensDao;
+begin
+  lSaidasItens := TSaidasItensDao.Create(vIConexao);
+  try
+    Result := lSaidasItens.ObterTotais(pNmeroSaida);
+
+    FTotalRecords := lSaidasItens.TotalRecords;
+  finally
+    lSaidasItens.Free;
+  end;
+end;
+
+function TSaidasItensModel.Salvar: String;
+var
+  lSaidasItensDao: TSaidasItensDao;
+begin
+  lSaidasItensDao := TSaidasItensDao.Create(vIConexao);
+  Result := '';
+  try
+    case FAcao of
+      Terasoft.Types.tacIncluir: Result := lSaidasItensDao.incluir(Self);
+      Terasoft.Types.tacAlterar: Result := lSaidasItensDao.alterar(Self);
+      Terasoft.Types.tacExcluir: Result := lSaidasItensDao.excluir(Self);
+    end;
+  finally
+    lSaidasItensDao.Free;
+  end;
+end;
+
+procedure TSaidasItensModel.SetAcao(const Value: TAcao);
+begin
+  FAcao := Value;
+end;
+
+procedure TSaidasItensModel.SetALTURA_M(const Value: Variant);
+begin
+  FALTURA_M := Value;
+end;
+
+procedure TSaidasItensModel.SetAVULSO(const Value: Variant);
+begin
+  FAVULSO := Value;
+end;
+
+procedure TSaidasItensModel.SetCODIGO_CLI(const Value: Variant);
+begin
+  FCODIGO_CLI := Value;
+end;
+
+procedure TSaidasItensModel.SetCODIGO_PRO(const Value: Variant);
+begin
+  FCODIGO_PRO := Value;
+  getDadosProduto;
+end;
+
+procedure TSaidasItensModel.SetCountView(const Value: String);
+begin
+  FCountView := Value;
+end;
+
+procedure TSaidasItensModel.SetDATA_CAD(const Value: Variant);
+begin
+  FDATA_CAD := Value;
+end;
+
+procedure TSaidasItensModel.SetDESCONTO_PED(const Value: Variant);
+begin
+  FDESCONTO_PED := Value;
+end;
+
+procedure TSaidasItensModel.SetICMS_SAI(const Value: Variant);
+begin
+  FICMS_SAI := Value;
+end;
+
+procedure TSaidasItensModel.SetID(const Value: Variant);
+begin
+  FID := Value;
+end;
+
+procedure TSaidasItensModel.SetIDRecordView(const Value: Integer);
+begin
+  FIDRecordView := Value;
+end;
+
+procedure TSaidasItensModel.SetIPI_SAI(const Value: Variant);
+begin
+  FIPI_SAI := Value;
+end;
+
+procedure TSaidasItensModel.SetLARGURA_M(const Value: Variant);
+begin
+  FLARGURA_M := Value;
+end;
+
+procedure TSaidasItensModel.SetLengthPageView(const Value: String);
+begin
+  FLengthPageView := Value;
+end;
+
+procedure TSaidasItensModel.SetLOJA(const Value: Variant);
+begin
+  FLOJA := Value;
+end;
+
+procedure TSaidasItensModel.SetNumeroSaidaView(const Value: String);
+begin
+  FNumeroSaidaView := Value;
+end;
+
+procedure TSaidasItensModel.SetNUMERO_SAI(const Value: Variant);
+begin
+  FNUMERO_SAI := Value;
+end;
+
+procedure TSaidasItensModel.SetOrderView(const Value: String);
+begin
+  FOrderView := Value
+end;
+
+procedure TSaidasItensModel.SetPRODUCAO_ID(const Value: Variant);
+begin
+  FPRODUCAO_ID := Value;
+end;
+
+procedure TSaidasItensModel.SetPROFUNDIDADE_M(const Value: Variant);
+begin
+  FPROFUNDIDADE_M := Value;
+end;
+
+procedure TSaidasItensModel.SetQTD_CHECAGEM(const Value: Variant);
+begin
+  FQTD_CHECAGEM := Value;
+end;
+
+procedure TSaidasItensModel.SetQUANTIDADE_ATE(const Value: Variant);
+begin
+  FQUANTIDADE_ATE := Value;
+end;
+
+procedure TSaidasItensModel.SetQUANTIDADE_SAI(const Value: Variant);
+begin
+  FQUANTIDADE_SAI := Value;
+end;
+
+procedure TSaidasItensModel.SetREPOSICAO_ID(const Value: Variant);
+begin
+  FREPOSICAO_ID := Value;
+end;
+
+procedure TSaidasItensModel.SetRESERVA_ID(const Value: Variant);
+begin
+  FRESERVA_ID := Value;
+end;
+
+procedure TSaidasItensModel.SetSAIDA_ID(const Value: Variant);
+begin
+  FSAIDA_ID := Value;
+end;
+
+procedure TSaidasItensModel.SetStartRecordView(const Value: String);
+begin
+  FStartRecordView := Value
+end;
+
+procedure TSaidasItensModel.SetSTATUS(const Value: Variant);
+begin
+  FSTATUS := Value;
+end;
+
+procedure TSaidasItensModel.SetSYSTIME(const Value: Variant);
+begin
+  FSYSTIME := Value;
+end;
+
+procedure TSaidasItensModel.SetTotalRecords(const Value: Integer);
+begin
+  FTotalRecords := Value
+end;
+
+procedure TSaidasItensModel.SetVALOR_UNI_SAI(const Value: Variant);
+begin
+  FVALOR_UNI_SAI := Value;
+end;
+
+procedure TSaidasItensModel.SetWhereView(const Value: String);
+begin
+  FWhereView := Value;
+end;
+
+end.
