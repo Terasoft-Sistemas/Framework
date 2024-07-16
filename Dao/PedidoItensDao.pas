65d57f50bdead964cf4f73ba09f8f9c81dcf4899
Alexandro Gotardo <ale_go121@hotmail.com>
2024-07-16 17:23:42 -0300

    Tarefa: 1364287

--- Dao/PedidoItensDao.pas
+++ Dao/PedidoItensDao.pas
@@ -336,7 +336,7 @@ begin
   try
     lSQL :=
       ' select sum( valorunitario_ped * qtde_calculada ) VALOR_TOTAL_ITENS,                                                                      '+
-      '        sum( qtde_calculada * coalesce(quantidade_tipo,0) ) VALOR_TOTAL_GARANTIA,                                                         '+
+      '        sum( (qtde_calculada * coalesce(quantidade_tipo,0)) + (qtde_calculada * coalesce(VLR_GARANTIA_FR,0)) ) VALOR_TOTAL_GARANTIA,      '+
       '        sum(round(cast(((cast(VALORUNITARIO_PED  as float) * desconto_ped / 100)) * qtde_calculada as float),2)) as VALOR_TOTAL_DESCONTO  '+
       '   from pedidoitens                                                                                                                       '+
       '  where numero_ped = '+QuotedStr(pNumeroPedido);
