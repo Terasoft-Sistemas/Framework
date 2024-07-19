74b932067e3e6745a4e9e14350d90420c7152ea1
Antonio S Schug <antoniosidineischug@gmail.com>
2024-07-19 13:44:57 -0300

    Update ClienteModel.pas

---
+++ Model/Terasoft.Model.Base.pas
@@ -0,0 +1,20 @@
+
+{$i definicoes.inc}
+
+unit Terasoft.Model.Base;
+interface
+  uses
+    Terasoft.Framework.DB,
+    Terasoft.Framework.Texto;
+
+  type
+    TModelBase = class
+
+    public
+
+    end;
+
+
+implementation
+
+end.
